56d6d799030b2862a54944dd6c9715a5f7afa8b2
NeverRmore <rosemissed@tutanota.com>
2024-06-27 18:48:02 +0300

    Add project files.

---
+++ CostManagement/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CostManagement.ViewModels;
+using System.Windows;
+
+namespace CostManagement.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as MainViewModel).AddExpense(txtDescription.Text, decimal.Parse(txtAmount.Text));
+            txtDescription.Clear();
+            txtAmount.Clear();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as MainViewModel).EditExpense((lvExpenses.SelectedItem as Expense));
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as MainViewModel).DeleteExpense((lvExpenses.SelectedItem as Expense));
+        }
+    }
+}
